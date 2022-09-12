99253a2e7fe4088052c47b976d855c3caec6bb3e
Alexander Bodrov <alexander.m.bodrov@gmail.com>
2022-09-12 13:49:17 +0300

    python script finished

--- feature/1.feature
+++ feature/1.feature
@@ -2,19 +2,15 @@ Feature: Test
 
     Scenario: Write test-case via Gherkin
         Given Open the browser and expand to full screen
-        Then Go to the yandex.ru page
-        And In the "Market" section choose "Smartphones"
-
-
+        When Go to the yandex.ru page
+        When In the "Market" section choose "Smartphones"
         When Open "All filters"
-        Then Choose price under 2000 rubles and screen size from 3 inches
-        And  Choose 5 producers
-
+        When Choose price under 2000 rubles and screen size from 3 inches
+        When  Choose 5 producers
         When Press the "Show" button
-        Then Count the number of producers on the page
-        And Remember the last smartphone in the page
-
+        When Count the number of producers on the page
+        When Remember the last smartphone in the page
         When Change sort by price
-        Then Find and click to the name of smartphone in the memory
-        And Show rating of the smartphone
-        And Quit the browser
+        When Find and click to the name of smartphone in the memory
+        When Show rating of the smartphone
+        Then Quit the browser
--- step_defs/step_defs.py
+++ step_defs/step_defs.py
@@ -1,92 +1,82 @@
-# import pytest_bdd
-# import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.ui import Select
-
-
 
 # Открыть браузер и развернуть на весь экран.
+
 browser = webdriver.Chrome()
 browser.implicitly_wait(10)
 browser.maximize_window()
 
-
 try:
-
-
-    #
-    # # Зайти на yandex.ru
-    # link = "https://market.yandex.ru/catalog--smartfony"
+    # Зайти на yandex.ru
     link = "https://yandex.ru/"
     browser.get(link)
 
     # В разделе «Маркет» выбрать «Смартфоны».
     browser.find_element(By.CLASS_NAME, "services-new__icon.services-new__icon_market").click()
-    new_window = browser.window_handles[1]
-    browser.switch_to.window(new_window)
-    browser.find_element(By.CSS_SELECTOR, "ul._381y5._21Njf > li:nth-child(9) > div").click()
-    browser.find_element(By.CSS_SELECTOR, "body > div._111XI.main > div:nth-child(5) > div > div > div:nth-child(2) > div > div:nth-child(2) > div > div > div > div > div:nth-child(1) > div > div > div > div > div:nth-child(2) > div > div:nth-child(1) > a > div > div").click()
+    latest_window = browser.window_handles[-1]
+    browser.close()
+    browser.switch_to.window(latest_window)
+    browser.find_element(By.ID, "catalogPopupButton").click()
+    browser.find_element(By.CSS_SELECTOR, "#catalogPopup > div > div > div > div > div > div > div:nth-child(1) > ul > li:nth-child(3) > a > span").click()
+    browser.find_element(By.CSS_SELECTOR, "div._111XI.main > div:nth-child(5) > div > div > div:nth-child(2) > div > div:nth-child(2) > div > div > div > div > div:nth-child(1) > div > div > div > div > div:nth-child(2) > div > div:nth-child(1) > a").click()
     browser.find_element(By.CSS_SELECTOR, "div._8Pmms.a6Vij > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div:nth-child(1) > a").click()
     # Перейти в «Все фильтры».
-    filters = browser.find_element(By.PARTIAL_LINK_TEXT, "Все фильтры")
-    browser.execute_script("return arguments[0].scrollIntoView(true);", filters)
-    filters.click()
-    # Задать параметр поиска до 20000 рублей и Диагональ экрана от 3 дюймов.
-    price = browser.find_element(By.CSS_SELECTOR, "div._3aaVQ > div:nth-child(1) > div:nth-child(1) div:nth-child(2) > input")
+    browser.find_element(By.CSS_SELECTOR, "div.kQnUO._2jRxX._1LZd1 > div > div:nth-child(5) > div > div > div > a").click()
+    # Задать параметр поиска до 20000 рублей
+    price = browser.find_element(By.CSS_SELECTOR, "div._3aaVQ > div:nth-child(1) > div:nth-child(1) > div > div > div > div:nth-child(2) > input")
     price.send_keys("20000")
-    browser.refresh()
+    # Задать Диагональ экрана от 3 дюймов.
+    screen_size_button = browser.find_element(By.CSS_SELECTOR, "div._3aaVQ > div:nth-child(1) > div:nth-child(12) > div > button")
+    browser.execute_script("arguments[0].scrollIntoView();", screen_size_button)
+    if "Диагональ экрана (точно)" in screen_size_button.text:
+        screen_size_button.click()
+        screen_size = browser.find_element(By.CSS_SELECTOR, "div._3aaVQ > div:nth-child(1) > div:nth-child(12) > div > div > div > div:nth-child(1) > input")
+        screen_size.send_keys("3")
+    else:
+        browser.find_element(By.CSS_SELECTOR, "div._3aaVQ > div:nth-child(1) > div:nth-child(11) > div > button").click()
+        screen_size = browser.find_element(By.CSS_SELECTOR, "div._3aaVQ > div:nth-child(1) > div:nth-child(11) > div > div > div > div:nth-child(1) > input")
+        screen_size.send_keys("3")
     # Выбрать не менее 5 любых производителей.
-    # time.sleep(10)
+    producers = browser.find_element(By.CSS_SELECTOR, "div._3aaVQ > div:nth-child(1) > div:nth-child(8) > div > button")
+    browser.execute_script("arguments[0].scrollIntoView();", producers)
+    time.sleep(2)
+    browser.find_element(By.CSS_SELECTOR, "div._307sS._2k6P8 > div > div:nth-child(2) > div > label").click()
+    browser.find_element(By.CSS_SELECTOR, "div._307sS._2k6P8 > div > div:nth-child(2) > div:nth-child(2) > label").click()
+    browser.find_element(By.CSS_SELECTOR, "div._307sS._2k6P8 > div > div:nth-child(2) > div:nth-child(4) > label").click()
+    browser.find_element(By.CSS_SELECTOR, "div._307sS._2k6P8 > div > div:nth-child(2) > div:nth-child(5) > label").click()
+    browser.find_element(By.CSS_SELECTOR, "div._307sS._2k6P8 > div > div:nth-child(2) > div:nth-child(6) > label").click()
+    browser.find_element(By.CSS_SELECTOR, "div._307sS._2k6P8 > div > div:nth-child(2) > div:nth-child(8) > label").click()
+    browser.find_element(By.CSS_SELECTOR, "div._307sS._2k6P8 > div > div:nth-child(2) > div:nth-child(9) > label").click()
+    # Нажать кнопку «Показать».
+    browser.find_element(By.CSS_SELECTOR, "a._2qvOO._3qN-v._1Rc6L").click()
+    # Посчитать кол-во смартфонов на одной странице.
+    end = browser.find_element(By.CLASS_NAME, "_2UK6L")
+    browser.execute_script("arguments[0].scrollIntoView(false);", end)
+    phones = browser.find_elements(By.CSS_SELECTOR, "h3._2UHry._2vVOc > a")
+    number_of_phones = len(phones)
+    print("number_of_phones", number_of_phones)
+    # Запомнить последний из списка.
+    phone = phones[-1].text
+    print("last phone", phone)
+    # Изменить Сортировку на другую (по цене/ по рейтингу/ по скидке).
+    sort_by_price_button = browser.find_element(By.CLASS_NAME, "_23p69._3D8AA")
+    browser.execute_script("arguments[0].scrollIntoView(true);", sort_by_price_button)
+    sort_by_price_button.click()
+    # Найти и нажать по имени запомненного объекта.
+    browser.refresh()
+    to_end = browser.find_element(By.CSS_SELECTOR, "h3._2UHry._2vVOc > a")
+    to_end.send_keys(Keys.END)
+    browser.find_element(By.LINK_TEXT, phone).click()
+    latest_window = browser.window_handles[-1]
+    browser.close()
+    browser.switch_to.window(latest_window)
+    rating = browser.find_element(By.CSS_SELECTOR, "div._1EOgH._2I6wc._1NfPD > span._2v4E8").text
+    print("smartphone_rating_is", rating)
 
-    producers = browser.find_element(By.CLASS_NAME, "_307sS._2k6P8")
-    browser.execute_script("return arguments[0].scrollIntoView(true);", producers)
-    # browser.find_element(By.CLASS_NAME, "_34OG2").click()
-    # time.sleep(3)
-    one = producers.find_elements(By.CLASS_NAME, "cyT3Q")
-    print(len(one))
-    # one.click()
-    # two = browser.find_element(By.XPATH, '/html/body/div[4]/section/div[2]/div/div/div[2]/div[1]/div[8]/div/div/div/div[2]/div[2]/label/div')
-    # two.click()
-    # three = producers.find_element(By.ID, "470013")
-    # three.click()
-    # four = producers.find_element(By.ID, "15292504")
-    # four.click()
-    # five = producers.find_element(By.ID, "963630")
-    # five.click()
-    # six = producers.find_element(By.ID, "153061")
-    # six.click()
-
-    # # Нажать кнопку «Показать».
-    # browser.find_element(By.CSS_SELECTOR, "body > div._111XI.main > section > div:nth-child(2) > div > div > div:nth-child(3) > div > div > a._2qvOO._3qN-v._1Rc6L").click()
-    # # Посчитать кол-во смартфонов на одной странице.
-    # phones = browser.find_elements(By.CSS_SELECTOR, "div.data-index")
-    # phone = browser.find_element(By.CSS_SELECTOR, "div.data-index")
-    # browser.execute_script("return arguments[0].scrollIntoView(true);", more)
-    # q = len(browser.find_elements(By.TAG_NAME, "h3"))
-
-    # for i in range(5):
-    #     for phone in phones:
-    #         # phone = browser.find_element(By.TAG_NAME, "h3")
-    #         phone.send_keys(Keys.PAGE_DOWN)
-    #         # browser.
-    #         # a = phone.find_element(By.CLASS_NAME, "page-cneevxtu00u > div > div > div > div > div:nth-child()") #page-cneevxtu00u > div > div > div > div > div:nth-child(9)
-    #         b = phone.get_attribute("data-item-index")
-    #         # a = a.__getattribute__("data-item-index")
-    #         time.sleep(1)
-    #         print(b)
-    # # for
-
-finally:
-    # browser.quit()
-    print("end")
-# # Запомнить последний из списка.
-# Изменить Сортировку на другую (по цене/ по рейтингу/ по
-# скидке).
-# Найти и нажать по имени запомненного объекта.
-# Вывести рейтинг выбранного товара.
 # Закрыть браузер.
+finally:
+    print("end")
+    browser.quit()
